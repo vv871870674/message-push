759f28a45f73edb238cd3109dcaa0832ec555c6a
DESKTOP-2SU0UF0\chenweiwei <871870674@qq.com>
2021-02-22 17:57:45 +0800

    提交项目架构

---
+++ src/test/java/com/official/messagepush/MessagePushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.official.messagepush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagePushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
