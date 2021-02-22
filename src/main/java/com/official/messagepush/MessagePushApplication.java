b5dbb48e1a14dd2b0dc328135b27a58891bfde2b
DESKTOP-2SU0UF0\chenweiwei <871870674@qq.com>
2021-02-22 20:37:44 +0800

    提交项目架构

--- src/main/java/com/official/messagepush/MessagePushApplication.java
+++ src/main/java/com/official/messagepush/MessagePushApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MessagePushApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MessagePushApplication.class, args);
     }
